3af30e6eea0bf38b7e6412465c02843d31f27ca1
radiophysiker <bolshakovdmitry@bk.ru>
2024-05-06 01:04:59 +0500

    refactor GetShortRandomString

--- internal/controller/http/v1/router.go
+++ internal/controller/http/v1/router.go
@@ -6,11 +6,6 @@ import (
 	"github.com/radiophysiker/link_shortener/internal/handlers"
 )
 
-type URL interface {
-	CreateShortURL(fullURL string) (string, error)
-	GetFullURL(shortURL string) (string, error)
-}
-
 // NewRouter creates a new router for the v1 API.
 func NewRouter(h *handlers.URLHandler) *chi.Mux {
 	r := chi.NewRouter()
--- internal/utils/get_short_random_string.go
+++ internal/utils/get_short_random_string.go
@@ -2,18 +2,16 @@ package utils
 
 import (
 	"math/rand"
-	"strings"
 )
 
-const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_"
+var alphabet = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_")
 
 func GetShortRandomString(countOfCharacters int) string {
 	countOfAlphabet := len(alphabet)
-	var encodedBuilder strings.Builder
-	encodedBuilder.Grow(countOfCharacters)
-	for range countOfCharacters {
-		encodedBuilder.WriteByte(alphabet[rand.Intn(countOfAlphabet)])
+	s := make([]rune, countOfCharacters)
+	for i := range countOfCharacters {
+		s[i] = alphabet[rand.Intn(countOfAlphabet)]
 	}
 
-	return encodedBuilder.String()
+	return string(s)
 }
